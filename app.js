2ded72d0e684d7f5e26d3e906adead973dac6f96
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-06 12:44:04 -0800

    saving repos to local storage for dev, will remove later

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const repos = [];
 
-fetch('https://api.github.com/users/themarquisdesheric/repos')
-  .then(res => res.json())
-  .then(githubRepos => repos.push(...githubRepos));
+// save to local storage for development
+const store = localStorage.getItem('repos');
+
+if (store) {
+  repos.push( ...JSON.parse(store) );
+} else {
+  fetch('https://api.github.com/users/themarquisdesheric/repos')
+    .then(res => res.json())
+    .then(githubRepos => repos.push(...githubRepos))
+    .then( () => 
+      localStorage.setItem('repos', JSON.stringify(repos))
+    );
+}
