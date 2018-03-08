711d97885af7bdbcb78fb1f744290149830d20b6
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-07 19:26:05 -0800

    tags are populating based on description

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const repos = [];
 const main = document.querySelector('main');
+const repos = [];
 
 // save to local storage for development
 const store = localStorage.getItem('repos');
@@ -18,20 +18,61 @@ if (store) {
 function makeCard(repo) {
   const card = document.createElement('section');
   const figure = document.createElement('figure');
+  const footer = document.createElement('footer');
   const p = document.createElement('p');
   
   p.textContent = repo.name;
-
+  
+  // create content for animated rollover if content available
   if (repo.description) {
-    // create content for animated rollover if content available
     const figcaption = document.createElement('figcaption');
-    figcaption.textContent = repo.description;
-    figure.appendChild(figcaption);
-  }
 
+    figcaption.textContent = repo.description;
+    figure.appendChild(figcaption);  
+  }
+  
+  footer.appendChild(p);
+  footer.appendChild(makeTags(repo.description));
   card.appendChild(figure);
-  card.appendChild(p);
+  card.appendChild(footer);
   main.appendChild(card);
+}
+
+function makeTags(description) {
+  const tags = document.createElement('div');
+
+  tags.classList.add('tags');
+  
+  description = description ? description.toLowerCase() : '';
+
+  // use HSL for color incrementing? 
+
+  if (description.includes('react')) tags.appendChild(makeTag('react'));
+  if (description.includes('redux')) tags.appendChild(makeTag('redux'));
+  if (description.includes('node')) tags.appendChild(makeTag('node'));
+  if (description.includes('jQuery')) tags.appendChild(makeTag('jQuery'));
+  if (description.includes('chart.js')) tags.appendChild(makeTag('chart.js'));
+  if (description.includes('mongo')) tags.appendChild(makeTag('mongo'));
+  if (description.includes('postgres')) tags.appendChild(makeTag('postgres'));
+  if (description.includes('mongoose')) tags.appendChild(makeTag('mongoose'));
+  if (description.includes('mocha')) tags.appendChild(makeTag('mocha'));
+  if (description.includes('jest')) tags.appendChild(makeTag('jest'));
+  if (description.includes('chai')) tags.appendChild(makeTag('chai'));
+  if (description.includes('express')) tags.appendChild(makeTag('express'));
+  if (description.includes('sass')) tags.appendChild(makeTag('sass'));
+  if (description.includes('react-router')) tags.appendChild(makeTag('react-router'));
+  if (description.includes('handlebars')) tags.appendChild(makeTag('handlebars'));
+
+  return tags;
+}
+
+function makeTag(text) {
+  const tag = document.createElement('span');
+  
+  tag.classList.add('tag');
+  tag.textContent = text;
+
+  return tag;
 }
 
 repos.forEach(makeCard);
