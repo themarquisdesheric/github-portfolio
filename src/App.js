4f7b3dc78cfdd233baa92f4d45683d29259daf62
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-12 20:15:29 -0700

    added sass; created Header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 const App = () => (
   <div id="wrapper">
-    <header>
-      <span className="header-text">t</span>
-      <span className="header-text">h</span>
-      <span className="header-text">e</span>
-      <span className="header-text">m</span>
-      <span className="header-text">a</span>
-      <span className="header-text">r</span>
-      <span className="header-text">q</span>
-      <span className="header-text">u</span>
-      <span className="header-text">i</span>
-      <span className="header-text">s</span>
-      <span className="header-text">d</span>
-      <span className="header-text">e</span>
-      <span className="header-text">s</span>
-      <span className="header-text">h</span>
-      <span className="header-text">e</span>
-      <span className="header-text">r</span>
-      <span className="header-text">i</span>
-      <span className="header-text">c</span>
-    </header>
+    <Header />
   </div>
 );
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <span className="header-text">t</span>
+    <span className="header-text">h</span>
+    <span className="header-text">e</span>
+    <span className="header-text">m</span>
+    <span className="header-text">a</span>
+    <span className="header-text">r</span>
+    <span className="header-text">q</span>
+    <span className="header-text">u</span>
+    <span className="header-text">i</span>
+    <span className="header-text">s</span>
+    <span className="header-text">d</span>
+    <span className="header-text">e</span>
+    <span className="header-text">s</span>
+    <span className="header-text">h</span>
+    <span className="header-text">e</span>
+    <span className="header-text">r</span>
+    <span className="header-text">i</span>
+    <span className="header-text">c</span>
+  </header>
+);
+
+export default Header;
