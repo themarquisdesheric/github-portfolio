287bc0cb5bcb86b440f18f1228176816902c4069
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-15 19:03:18 -0700

    have language breakdown for each repo, need to calculate

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GitHub from 'github-api';
 import Card from './Card';
+import fetch from 'isomorphic-fetch';
 
 class CardContainer extends React.Component {
   state = {
@@ -8,7 +9,7 @@ class CardContainer extends React.Component {
   };
 
   componentDidMount() {
-    const store = JSON.parse(localStorage.getItem('repos'));
+    const store = JSON.parse(sessionStorage.getItem('repos'));
 
     if (store) {
       this.setState({ repos: store });
@@ -16,14 +17,25 @@ class CardContainer extends React.Component {
       const gh = new GitHub({
         token: process.env.REACT_APP_GITHUB_KEY
       });
-      
+
       gh.getUser()
         .listRepos()
         .then(repos => repos.data.filter(repo => repo.owner.login === 'themarquisdesheric'))
         .then(repos => {
-          localStorage.setItem('repos', JSON.stringify(repos));
+          const headers = new Headers({
+            Authorization: `token ${process.env.REACT_APP_GITHUB_KEY}`,
+          });
 
+          // save to session storage in case of page reloads... don't want to tax the API
+          sessionStorage.setItem('repos', JSON.stringify(repos));
           this.setState({ repos });
+
+          const promises = repos.map(repo => 
+            fetch(repo.languages_url, { headers })
+              .then(res => res.json())
+          );
+          
+          const resolvedPromises = Promise.all(promises);
         });
     }
   }
