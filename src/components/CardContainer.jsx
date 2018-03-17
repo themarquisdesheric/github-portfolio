e0915bf1f7e6bc68a760e0b56b005f37826ac5eb
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-16 21:38:46 -0700

    implemented utility functions

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import GitHub from 'github-api';
-import Card from './Card';
 import fetch from 'isomorphic-fetch';
+import Card from './Card';
+import { calcLanguageTotals, calcRepoTotal } from '../utilities';
 
 class CardContainer extends React.Component {
   state = {
@@ -11,31 +12,56 @@ class CardContainer extends React.Component {
   componentDidMount() {
     const store = JSON.parse(sessionStorage.getItem('repos'));
 
-    if (store) {
-      this.setState({ repos: store });
-    } else {
+    if (store) this.setState({ repos: store });
+    
+    else {
       const gh = new GitHub({
         token: process.env.REACT_APP_GITHUB_KEY
       });
 
       gh.getUser()
         .listRepos()
-        .then(repos => repos.data.filter(repo => repo.owner.login === 'themarquisdesheric'))
+        .then(repos => repos.data.filter(repo => 
+          repo.owner.login === 'themarquisdesheric' && 
+          repo.name !== 'incubator-datafu'))
         .then(repos => {
-          const headers = new Headers({
-            Authorization: `token ${process.env.REACT_APP_GITHUB_KEY}`,
+
+          // display repos, then calculate language totals
+          this.setState({ repos }, () => {
+            const headers = new Headers({
+              Authorization: `token ${process.env.REACT_APP_GITHUB_KEY}`,
+            });
+            const totals = {
+              total: 0
+            };
+  
+            // get language statistics for each repo
+            const promises = repos.map(repo => 
+              fetch(repo.languages_url, { headers })
+                .then(res => res.json())
+                .then(repoStats => {
+                  calcLanguageTotals(repoStats, totals);
+                  
+                  const repoTotal = calcRepoTotal(repoStats);
+  
+                  totals.total += repoTotal;
+  
+                  // add language stats and total to repo
+                  repoStats.total = repoTotal;
+                  repo.languages = repoStats;
+  
+                  return repo;
+                })
+            );
+            
+            Promise.all(promises)
+              .then(repos => {
+                // save to session storage so page reloads don't affect rate limit
+                sessionStorage.setItem('repos', JSON.stringify(repos));
+      
+                this.setState({ repos, totals });
+              });
           });
-
-          // save to session storage in case of page reloads... don't want to tax the API
-          sessionStorage.setItem('repos', JSON.stringify(repos));
-          this.setState({ repos });
-
-          const promises = repos.map(repo => 
-            fetch(repo.languages_url, { headers })
-              .then(res => res.json())
-          );
-          
-          const resolvedPromises = Promise.all(promises);
         });
     }
   }
